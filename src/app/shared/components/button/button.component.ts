dade35ea6aab7b43c8067f5ea71fc42a441d88e6
Yury <yury.s.nazarov@yandex.ru>
2026-02-27 19:38:10 +0300

    3-5-3: Двухсторонняя привязка

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, ViewEncapsulation} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 // Декоратор добавляет мета данные класса
 // Это функция, которая принимает в себя объект, с обязательными параметрами
@@ -9,6 +10,7 @@ import {Component, Input, ViewEncapsulation} from '@angular/core';
   // encapsulation: ViewEncapsulation.None      // Отключает инкапсуляцию стилей в ангуляр, позволяя задавать селекторы на прямую в html DOM
   // encapsulation: ViewEncapsulation.ShadowDom // Инкапсуляция стилей применяется к конкретному элементу, а не всей странице через head
   encapsulation: ViewEncapsulation.Emulated,    // Используется по умолчанию
+  imports: [FormsModule],                       // Содержит весь набор функций и компонентов необходимых для работы форм
 })
 export class ButtonComponent {
   @Input() title = '';
