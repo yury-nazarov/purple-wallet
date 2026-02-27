eb53c6fe4540e2a7292473156d1710cb15139973
Yury <yury.s.nazarov@yandex.ru>
2026-02-27 20:08:20 +0300

    cleaning

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -7,19 +7,9 @@ import { FormsModule } from '@angular/forms';
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
-  // encapsulation: ViewEncapsulation.None      // Отключает инкапсуляцию стилей в ангуляр, позволяя задавать селекторы на прямую в html DOM
-  // encapsulation: ViewEncapsulation.ShadowDom // Инкапсуляция стилей применяется к конкретному элементу, а не всей странице через head
-  encapsulation: ViewEncapsulation.Emulated,    // Используется по умолчанию
-  imports: [FormsModule],                       // Содержит весь набор функций и компонентов необходимых для работы форм
 })
 export class ButtonComponent {
   @Input() title = '';
-  value: string = "";
-
-  onClickEvent(event: Event) {
-    console.log("Event", event);
-    this.value = "Новое значение";
-  }
 }
 
 
