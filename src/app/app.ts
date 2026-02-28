9b1e67f750af3a4e0b0f0cc26b9481fc6d31745d
Yury <yury.s.nazarov@gmail.com>
2026-02-28 21:49:09 +0300

    3-18: Подготовка к верстке layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,23 +1,12 @@
 import { Component, signal } from '@angular/core';
-import {ButtonComponent} from './shared/components/button/button.component';
-import {InputComponent} from './shared/components/input/input.component';
-import {PasswordInputComponent} from './shared/components/password-input/password-input.component';
+import {PublicLayoutComponent} from './public/_layout/layout.componrnt';
+
 
 @Component({
   selector: 'app-root',
-  imports: [ButtonComponent, InputComponent, PasswordInputComponent],
+  imports: [PublicLayoutComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('purple-wallet');
-
-  onBtnClick(): void {
-    console.log('btn Click');
-  }
-
-  onInputChange($event: string | null) {
-    console.log('inputChange', $event);
-  }
-}
+export class App {}
 
---
+++ src/app/public/_layout/layout.componrnt.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import {ButtonComponent} from '../../shared/components/button/button.component';
+import {PasswordInputComponent} from '../../shared/components/password-input/password-input.component';
+import {InputComponent} from '../../shared/components/input/input.component';
+
+@Component({
+  selector: 'app-layout',
+  imports: [ButtonComponent, InputComponent, PasswordInputComponent],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+  standalone: true,
+})
+export class PublicLayoutComponent {
+
+  onInputChange($event: string | null) {
+
+  }
+}
