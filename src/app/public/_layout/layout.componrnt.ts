e84c32f4ddf572d7d5df47a227d157749a24dfc4
Yury <yury.s.nazarov@gmail.com>
2026-02-28 22:34:03 +0300

    3-18: Сверстанный layout

--- src/app/public/_layout/layout.componrnt.ts
+++ src/app/public/_layout/layout.componrnt.ts
@@ -2,10 +2,11 @@ import {Component} from '@angular/core';
 import {ButtonComponent} from '../../shared/components/button/button.component';
 import {PasswordInputComponent} from '../../shared/components/password-input/password-input.component';
 import {InputComponent} from '../../shared/components/input/input.component';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-layout',
-  imports: [ButtonComponent, InputComponent, PasswordInputComponent],
+  imports: [ButtonComponent, InputComponent, PasswordInputComponent, NgOptimizedImage],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
   standalone: true,
