b29275e69cce68099b862587280f1384c2f7a7f4
Yury <yury.s.nazarov@gmail.com>
2026-03-04 23:00:44 +0300

    4-2: RouterLink и переход по кнопке 'Войти'

--- src/app/public/_layout/layout.componrnt.ts
+++ src/app/public/_layout/layout.componrnt.ts
@@ -3,10 +3,11 @@ import {ButtonComponent} from '../../shared/components/button/button.component';
 import {PasswordInputComponent} from '../../shared/components/password-input/password-input.component';
 import {InputComponent} from '../../shared/components/input/input.component';
 import {NgOptimizedImage} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [ButtonComponent, InputComponent, PasswordInputComponent, NgOptimizedImage],
+  imports: [ButtonComponent, InputComponent, PasswordInputComponent, NgOptimizedImage, RouterLink],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
   standalone: true,
