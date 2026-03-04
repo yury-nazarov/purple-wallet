44cca12d23236a26191684413a8269563b662144
Yury <yury.s.nazarov@gmail.com>
2026-03-04 23:40:59 +0300

    4-4: Программная навигация через click

--- src/app/public/_layout/layout.componrnt.ts
+++ src/app/public/_layout/layout.componrnt.ts
@@ -1,9 +1,9 @@
-import {inject, Component, OnInit} from '@angular/core';
+import {inject, Component} from '@angular/core';
 import {ButtonComponent} from '../../shared/components/button/button.component';
 import {PasswordInputComponent} from '../../shared/components/password-input/password-input.component';
 import {InputComponent} from '../../shared/components/input/input.component';
 import {NgOptimizedImage} from '@angular/common';
-import {ActivatedRoute, RouterLink} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -14,7 +14,16 @@ import {ActivatedRoute, RouterLink} from '@angular/router';
 })
 export class PublicLayoutComponent {
 
+  public router: Router = inject(Router);
 
   onInputChange($event: string | null) {}
 
+  onLoginBtnClick() {
+    console.log('onLoginBtnClick');
+    this.router.navigate(['/private'], {
+      queryParams: {
+        from: 'auth',
+      }
+    });
+  }
 }
