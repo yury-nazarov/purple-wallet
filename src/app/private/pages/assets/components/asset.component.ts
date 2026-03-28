863fb8d6c43d63311c8a1ed5f904b43930a2d6d6
Yury <yury.s.nazarov@gmail.com>
2026-03-28 10:33:51 +0300

    6: Пайпы: Пример

--- src/app/private/pages/assets/components/asset.component.ts
+++ src/app/private/pages/assets/components/asset.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { IAssetModel } from '../models/asset.model';
-import { NgClass, NgOptimizedImage, UpperCasePipe } from '@angular/common';
+import { CurrencyPipe, DecimalPipe, NgClass, NgOptimizedImage, PercentPipe, UpperCasePipe } from '@angular/common';
 
 
 @Component({
@@ -8,7 +8,7 @@ import { NgClass, NgOptimizedImage, UpperCasePipe } from '@angular/common';
   templateUrl: './asset.component.html',
   styleUrls: ['./asset.component.scss'],
   standalone: true,
-  imports: [NgOptimizedImage, NgClass, UpperCasePipe],
+  imports: [NgOptimizedImage, NgClass, UpperCasePipe, CurrencyPipe, PercentPipe, DecimalPipe],
 })
 export class AssetComponent {
   // Принимаем в компонент данные для элементов отдельнойвалюты
