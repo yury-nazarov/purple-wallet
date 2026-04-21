e0cf69427cd4c3b33e43a481cf5b04e053018c70
Yury <yury.s.nazarov@gmail.com>
2026-04-21 07:23:07 +0300

    7-1: Пример асинхронности с Signals

--- src/app/private/pages/rates/rates.component.ts
+++ src/app/private/pages/rates/rates.component.ts
@@ -1,24 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, effect, OnInit, signal } from '@angular/core';
 import { IRateModel } from './models/rate.model';
 import { FAKE_RATES } from '../../../shared/const/fake-rates.const';
 import { RateComponent } from './components/rate/rate.component';
-import { delay, Observable, of } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-rates',
   templateUrl: './rates.component.html',
   styleUrls: ['./rates.component.scss'],
   standalone: true,
-  imports: [RateComponent, AsyncPipe],
+  imports: [RateComponent],
 })
 export class RatesComponent implements OnInit {
-  //rates: IRateModel[] = FAKE_RATES;
-  rates$: Observable<IRateModel[]> = of(FAKE_RATES).pipe(delay(1000));
+  rates = signal<IRateModel[]>([]);
+
+  constructor() {
+    // Подписка на изменения
+    effect(() => {
+      console.log('Rates update:', this.rates());
+    });
+  }
 
   ngOnInit() {
-    // В этом случае нужно не забыть выполнить самостоятельные отписки через OnDestroy lifecycle hook
-    // Иначе будут утечки памяти
-    this.rates$.subscribe(console.log);
+    setTimeout(() => {
+      this.rates.set(FAKE_RATES);
+    }, 1000);
   }
 }
