1fc25769f349a118bb2ce9f0953a661503898b94
Yury <yury.s.nazarov@gmail.com>
2026-04-21 07:11:55 +0300

    7-1: Пример асинхронности с RxJS

--- src/app/private/pages/rates/rates.component.ts
+++ src/app/private/pages/rates/rates.component.ts
@@ -1,15 +1,24 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IRateModel } from './models/rate.model';
 import { FAKE_RATES } from '../../../shared/const/fake-rates.const';
 import { RateComponent } from './components/rate/rate.component';
+import { delay, Observable, of } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-rates',
   templateUrl: './rates.component.html',
   styleUrls: ['./rates.component.scss'],
   standalone: true,
-  imports: [RateComponent],
+  imports: [RateComponent, AsyncPipe],
 })
-export class RatesComponent {
-  rates: IRateModel[] = FAKE_RATES;
+export class RatesComponent implements OnInit {
+  //rates: IRateModel[] = FAKE_RATES;
+  rates$: Observable<IRateModel[]> = of(FAKE_RATES).pipe(delay(1000));
+
+  ngOnInit() {
+    // В этом случае нужно не забыть выполнить самостоятельные отписки через OnDestroy lifecycle hook
+    // Иначе будут утечки памяти
+    this.rates$.subscribe(console.log);
+  }
 }
